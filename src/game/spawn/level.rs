897c368a4b7875ad869c924e5083a7a006b0cf6d
Jan Hohenheim <jan@hohenheim.ch>
2024-07-21 03:38:50 +0200

    Add bevy_inspector_egui

--- src/game/spawn/level.rs
+++ src/game/spawn/level.rs
@@ -15,6 +15,7 @@ pub struct SpawnLevel;
 fn spawn_level(_trigger: Trigger<SpawnLevel>, mut commands: Commands) {
     // here we spawn our game world/level, which is also a blueprint !
     commands.spawn((
+        Name::new("Level"),
         BlueprintInfo::from_path("levels/World.glb"), // all we need is a Blueprint info...
         SpawnBlueprint, // and spawnblueprint to tell blenvy to spawn the blueprint now
         HideUntilReady, // only reveal the level once it is ready
