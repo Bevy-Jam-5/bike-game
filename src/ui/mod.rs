7cdf7aca7f1ba306c93d46abacd15112c0409d89
Jan Hohenheim <jan@hohenheim.ch>
2024-07-24 01:36:09 +0200

    Lock cursor while playing

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -6,6 +6,7 @@
 pub mod interaction;
 pub mod palette;
 mod widgets;
+mod cursor;
 
 pub mod prelude {
     pub use super::{
@@ -18,5 +19,5 @@ pub mod prelude {
 use bevy::prelude::*;
 
 pub(super) fn plugin(app: &mut App) {
-    app.add_plugins(interaction::plugin);
+    app.add_plugins((interaction::plugin, cursor::plugin));
 }
