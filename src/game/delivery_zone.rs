8bb19c6da0d3b1caf723bfa4d8efb5008c440494
Jan Hohenheim <jan@hohenheim.ch>
2024-07-25 02:22:48 +0200

    Make player able to enter zone

--- src/game/delivery_zone.rs
+++ src/game/delivery_zone.rs
@@ -1,6 +1,9 @@
 use avian3d::prelude::*;
 use bevy::prelude::*;
 
+use crate::game::spawn::player::Player;
+use crate::util::single;
+
 pub(super) fn plugin(app: &mut App) {
     app.register_type::<DeliveryZone>();
     app.add_systems(Update, on_delivery_player_collision);
@@ -10,4 +13,14 @@ pub(super) fn plugin(app: &mut App) {
 #[reflect(Debug, Component, PartialEq)]
 struct DeliveryZone;
 
-fn on_delivery_player_collision(q_delivery_zone: Query<&CollidingEntities, With<DeliveryZone>>) {}
+fn on_delivery_player_collision(
+    q_delivery_zone: Query<&CollidingEntities, With<DeliveryZone>>,
+    q_player: Query<Entity, With<Player>>,
+) {
+    let player = single!(q_player);
+    for delivery_zone in q_delivery_zone.iter() {
+        if delivery_zone.0.contains(&player) {
+            info!("Player entered delivery zone");
+        }
+    }
+}
