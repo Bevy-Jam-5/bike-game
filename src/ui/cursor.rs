8ac0aacb8c8cb500298f133f469cde34ca7ca7c5
Jan Hohenheim <jan@hohenheim.ch>
2024-07-25 20:35:51 +0200

    Re-add system

--- src/ui/cursor.rs
+++ src/ui/cursor.rs
@@ -3,7 +3,7 @@ use bevy::{prelude::*, window::CursorGrabMode};
 use crate::{screen::Screen, util::single_mut};
 
 pub(super) fn plugin(app: &mut App) {
-    //app.add_systems(OnEnter(Screen::Playing), capture_cursor);
+    app.add_systems(OnEnter(Screen::Playing), capture_cursor);
     app.add_systems(OnExit(Screen::Playing), release_cursor);
 }
 
