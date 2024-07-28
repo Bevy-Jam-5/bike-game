bb3550f6b9ba828cde933a2e02128e6995ab6f86
Jan Hohenheim <jan@hohenheim.ch>
2024-07-28 05:35:21 +0200

    Improve Wasm hacks

--- src/game/spawn/level.rs
+++ src/game/spawn/level.rs
@@ -1,17 +1,22 @@
 //! Spawn the main level by triggering other observers.
 
-use bevy::{color::palettes::tailwind, prelude::*};
+use bevy::{
+    color::palettes::tailwind,
+    prelude::*,
+    utils::{HashMap, HashSet},
+};
 use blenvy::*;
 
-use crate::{
-    screen::{PlayState, Screen},
-    util::single,
-};
+use crate::screen::{PlayState, Screen};
 
 pub(super) fn plugin(app: &mut App) {
     app.observe(spawn_level);
-    app.observe(on_level_loaded);
-    app.add_systems(Update, hack_loading.run_if(in_state(PlayState::Spawning)));
+    app.add_systems(
+        Update,
+        (hack_loading, on_level_loaded)
+            .chain()
+            .run_if(in_state(PlayState::Spawning)),
+    );
 }
 
 #[derive(Event, Debug)]
@@ -33,15 +38,12 @@ fn spawn_level(_trigger: Trigger<SpawnLevel>, mut commands: Commands) {
 }
 
 fn on_level_loaded(
-    trigger: Trigger<OnAdd, BlueprintInstanceReady>,
-    q_world: Query<&GameWorldTag>,
+    q_world: Query<&GameWorldTag, With<BlueprintInstanceReady>>,
     mut next_state: ResMut<NextState<PlayState>>,
 ) {
-    let entity = trigger.entity();
-    if !q_world.contains(entity) {
+    if q_world.is_empty() {
         return;
     }
-
     next_state.set(PlayState::Active);
 }
 
@@ -49,19 +51,66 @@ fn on_level_loaded(
 /// due to a bug that is probably coming from Blenvy
 fn hack_loading(
     mut commands: Commands,
-    q_world: Query<
+    q_loading: Query<
         Entity,
         (
-            With<GameWorldTag>,
             With<BlueprintSpawning>,
             Without<BlueprintReadyForFinalizing>,
             Without<BlueprintInstanceReady>,
         ),
     >,
-    q_blueprints: Query<Has<BlueprintInstanceReady>, (With<BlueprintInfo>, Without<GameWorldTag>)>,
+    q_children: Query<&Children>,
+    q_blueprints: Query<Has<BlueprintInstanceReady>, With<BlueprintInfo>>,
 ) {
-    let world = single!(q_world);
-    if !q_blueprints.is_empty() && q_blueprints.iter().all(|ready| ready) {
-        commands.entity(world).insert(BlueprintReadyForFinalizing);
+    if q_loading.is_empty() {
+        return;
+    }
+    let mut processed = HashSet::new();
+    let mut ready_map = HashMap::new();
+    for loading in q_loading.iter() {
+        if processed.contains(&loading) {
+            continue;
+        }
+        go_through_children(
+            &mut commands,
+            &q_children,
+            &q_blueprints,
+            loading,
+            &mut ready_map,
+        );
+        processed.insert(loading);
+    }
+}
+
+fn go_through_children(
+    commands: &mut Commands,
+    q_children: &Query<&Children>,
+    q_blueprints: &Query<Has<BlueprintInstanceReady>, With<BlueprintInfo>>,
+    entity: Entity,
+    ready_map: &mut HashMap<Entity, bool>,
+) -> bool {
+    if q_blueprints.contains(entity) && !q_children.contains(entity) {
+        ready_map.insert(entity, false);
+        return false;
+    }
+    match q_children.get(entity) {
+        Ok(children) => {
+            let ready = children.iter().all(|child| {
+                if let Some(ready) = ready_map.get(child) {
+                    *ready
+                } else {
+                    let ready =
+                        go_through_children(commands, q_children, q_blueprints, *child, ready_map);
+                    ready_map.insert(*child, ready);
+                    ready
+                }
+            });
+            if ready {
+                ready_map.insert(entity, true);
+                commands.entity(entity).insert(BlueprintReadyForFinalizing);
+            }
+            ready
+        }
+        Err(_) => true,
     }
 }
