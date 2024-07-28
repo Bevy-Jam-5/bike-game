13d1816282726b564b95d18b4c5dab4dbe990d13
Jan Hohenheim <jan@hohenheim.ch>
2024-07-28 03:59:47 +0200

    Fix cursor lock on Wasm (#20)

--- src/ui/cursor.rs
+++ src/ui/cursor.rs
@@ -4,14 +4,34 @@ use crate::{screen::PlayState, util::single_mut};
 
 pub(super) fn plugin(app: &mut App) {
     #[cfg(not(feature = "dev"))]
-    app.add_systems(OnEnter(PlayState::Active), capture_cursor);
+    {
+        #[cfg(not(target_arch = "wasm32"))]
+        {
+            app.add_systems(OnEnter(PlayState::Active), capture_cursor);
+        }
+        #[cfg(target_arch = "wasm32")]
+        {
+            // Running this continuously due to https://github.com/TheBevyFlock/bevy_quickstart/issues/198
+            app.add_systems(Update, capture_cursor.run_if(in_state(PlayState::Active)));
+        }
+    }
     app.add_systems(OnExit(PlayState::Active), release_cursor);
 }
 
 pub fn capture_cursor(mut q_window: Query<&mut Window>) {
     let mut window = single_mut!(q_window);
     window.cursor.visible = false;
-    window.cursor.grab_mode = CursorGrabMode::Locked;
+    #[cfg(not(target_arch = "wasm32"))]
+    {
+        window.cursor.grab_mode = CursorGrabMode::Locked;
+    }
+    #[cfg(target_arch = "wasm32")]
+    {
+        // Confined actually resets the mouse position,
+        // but only works when paired with a click.
+        // So we run this every frame.
+        window.cursor.grab_mode = CursorGrabMode::Confined;
+    }
 }
 
 pub fn release_cursor(mut q_window: Query<&mut Window>) {
