4efdec5b4f6119c95bd9a0aaaadd59479889ff71
Joona Aalto <jondolf.dev@gmail.com>
2024-07-29 19:13:56 +0300

    Rework time and game end

--- src/game/time.rs
+++ src/game/time.rs
@@ -11,12 +11,13 @@ use super::spawn::hud::TimeText;
 
 pub fn plugin(app: &mut App) {
     app.init_resource::<InGameTime>()
-        .register_type::<InGameTime>();
+        .init_resource::<RemainingTime>()
+        .register_type::<(InGameTime, RemainingTime)>();
 
     app.add_systems(
         Update,
         (
-            tick_in_game_time,
+            tick_time,
             update_time_text.run_if(resource_changed::<InGameTime>),
         )
             .chain()
@@ -24,24 +25,44 @@ pub fn plugin(app: &mut App) {
     );
 
     // Leaving the gameplay screen currently resets the world, so reset the timer.
-    app.add_systems(OnExit(Screen::Playing), reset_in_game_time);
+    app.add_systems(OnExit(Screen::Playing), reset_time);
 }
+
+/// How much time the player has at the start of the game.
+pub const INITIAL_TIME: Duration = Duration::from_secs(60);
 
 #[derive(Debug, Resource, Clone, Default, Deref, DerefMut, PartialEq, Reflect)]
 #[reflect(Debug, Resource, Default, PartialEq)]
 pub struct InGameTime(pub Stopwatch);
 
-fn update_time_text(time: Res<InGameTime>, mut time_text: Query<&mut Text, With<TimeText>>) {
+#[derive(Debug, Resource, Clone, Deref, DerefMut, PartialEq, Reflect)]
+#[reflect(Debug, Resource, Default, PartialEq)]
+pub struct RemainingTime(pub Duration);
+
+impl Default for RemainingTime {
+    fn default() -> Self {
+        Self(INITIAL_TIME)
+    }
+}
+
+fn update_time_text(time: Res<RemainingTime>, mut time_text: Query<&mut Text, With<TimeText>>) {
     let mut text = single_mut!(time_text);
-    text.sections[1].value = format_duration_to_mm_ss(time.elapsed());
+    text.sections[1].value = format_duration_to_mm_ss(time.0);
 }
 
-fn tick_in_game_time(mut in_game_time: ResMut<InGameTime>, time: Res<Time>) {
-    in_game_time.tick(time.delta());
+fn tick_time(
+    mut in_game_time: ResMut<InGameTime>,
+    mut remaining_time: ResMut<RemainingTime>,
+    time: Res<Time>,
+) {
+    let delta = time.delta();
+    in_game_time.tick(delta);
+    remaining_time.0 = remaining_time.saturating_sub(delta);
 }
 
-fn reset_in_game_time(mut in_game_time: ResMut<InGameTime>) {
+fn reset_time(mut in_game_time: ResMut<InGameTime>, mut remaining_time: ResMut<RemainingTime>) {
     in_game_time.reset();
+    *remaining_time = RemainingTime::default();
 }
 
 /// Returns a time string in the MM:SS format for the given duration.
