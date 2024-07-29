7784f21dc94942a9da7997fc3ea5296e94b6a3c9
Jan Hohenheim <jan@hohenheim.ch>
2024-07-29 15:27:07 +0200

    Fix asset loading (#37)
    Co-authored-by: Tau Gärtli <git@tau.garden>

--- src/screen/playing/mod.rs
+++ src/screen/playing/mod.rs
@@ -1,6 +1,8 @@
 //! The screen state for the main game loop.
 
-use bevy::{input::common_conditions::input_just_pressed, prelude::*};
+#[cfg(feature = "dev")]
+use bevy::input::common_conditions::input_just_pressed;
+use bevy::prelude::*;
 
 use super::{PlayState, Screen};
 use crate::game::audio::soundtrack::PlaySoundtrack;
@@ -12,11 +14,14 @@ pub mod spawning;
 pub(super) fn plugin(app: &mut App) {
     app.add_systems(OnExit(Screen::Playing), exit_playing);
 
-    app.add_systems(
-        Update,
-        return_to_title_screen
-            .run_if(in_state(Screen::Playing).and_then(input_just_pressed(KeyCode::Escape))),
-    );
+    #[cfg(feature = "dev")]
+    {
+        app.add_systems(
+            Update,
+            return_to_title_screen
+                .run_if(in_state(Screen::Playing).and_then(input_just_pressed(KeyCode::Escape))),
+        );
+    }
     app.add_plugins((spawning::plugin, loading_pipelines::plugin, active::plugin));
 }
 
@@ -25,6 +30,7 @@ fn exit_playing(mut commands: Commands) {
     commands.trigger(PlaySoundtrack::Disable);
 }
 
+#[cfg(feature = "dev")]
 fn return_to_title_screen(mut next_screen: ResMut<NextState<Screen>>) {
     next_screen.set(Screen::Title);
 }
